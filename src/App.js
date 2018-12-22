05267ff4910c5650851edfb5917481934a3a6a85
LAPTOP-AD9KDN1M\Tatyana <tat.batishcheva@gmail.com>
2018-12-22 12:51:09 +0300

    Add 3 buttons and div

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      background: 'App-background-red',
+    };
+  }
+
+  handleClick = (color) => {
+    this.setState({
+      background: color,
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className={this.state.background}>Hello World!</div>
+          <input onClick={() => this.handleClick('App-background-red')} type="button" value="red" />
+          <input onClick={() => this.handleClick('App-background-green')} type="button" value="green" />
+          <input onClick={() => this.handleClick('App-background-blue')} type="button" value="blue" />
         </header>
       </div>
     );
